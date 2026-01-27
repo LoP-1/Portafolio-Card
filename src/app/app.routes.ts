87f84262ca899e2d90ce075b6287dbc33fe4e29d
LoP-1 <miguel.castillo.valero@gmail.com>
2026-01-27 04:45:20 -0500

    fondo de pagina principal creado - apartado de infomacion avanzado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Informacion} from './informacion/informacion';
+import { ignoreElements } from 'rxjs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Informacion
+  /* { path: 'proyectos', component: ProyectosComponent },
+  { path: 'habilidades', component: SkillsComponent } 
+  */}
+];
