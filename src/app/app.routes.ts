ff5bb6dc1ccdbb9e2e64a95c08fab5f2c7c7478d
LoP-1 <miguel.castillo.valero@gmail.com>
2026-01-29 21:28:45 -0500

    apartado proyectos avanzado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { Informacion} from './informacion/informacion';
-import { ignoreElements } from 'rxjs';
+import { Informacion } from './informacion/informacion';
+import { Experiencia } from './experiencia/experiencia';
+// import { Proyectos } from './proyectos/proyectos';
+// import { Contacto } from './contacto/contacto';
 
 export const routes: Routes = [
-  { path: '', component: Informacion
-  /* { path: 'proyectos', component: ProyectosComponent },
-  { path: 'habilidades', component: SkillsComponent } 
-  */}
+  { path: '', component: Informacion },
+  { path: 'experiencia', component: Experiencia },
+  { path: 'proyectos', component: Experiencia }, 
+  { path: 'contacto', component: Experiencia }
 ];
