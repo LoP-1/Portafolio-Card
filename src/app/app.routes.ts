daba6f602b4d795fe291a9bd328b9d9d0bf08b08
LoP-1 <miguel.castillo.valero@gmail.com>
2026-01-30 19:21:37 -0500

    apartado proyectos agregado - apartado experiencia completado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Informacion } from './informacion/informacion';
 import { Experiencia } from './experiencia/experiencia';
-// import { Proyectos } from './proyectos/proyectos';
+import { Proyectos } from './proyectos/proyectos';
 // import { Contacto } from './contacto/contacto';
 
 export const routes: Routes = [
   { path: '', component: Informacion },
   { path: 'experiencia', component: Experiencia },
-  { path: 'proyectos', component: Experiencia }, 
+  { path: 'proyectos', component: Proyectos }, 
   { path: 'contacto', component: Experiencia }
 ];
