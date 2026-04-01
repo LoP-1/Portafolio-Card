8d6694b32c410688658cdb2fac15302bc275b286
LoP-1 <miguel.castillo.valero@gmail.com>
2026-04-01 03:39:34 -0500

    paleta con 2 colores nuevos

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,13 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('portafolio-card');
+  
+  themes = ['', 'theme-gameboy', 'theme-hacker'];
+  currentThemeIndex = signal(0);
+
+  toggleTheme() {
+    this.currentThemeIndex.update(i => (i + 1) % this.themes.length);
+    const newTheme = this.themes[this.currentThemeIndex()];
+    document.body.className = newTheme;
+  }
 }
